3fce580b59700a0e805dbc0c068292b47a7a871d
AmIEviil <lokiinn@gmail.com>
2021-10-27 13:01:43 -0300

    confirmar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { LoadingController } from '@ionic/angular';
 import { MarcadorI } from '../model/marcador.interface';
+import { WayPoint } from '../model/WayPoints';
+import { ApiService } from '../api.service';
 declare var google;
 @Component({
   selector: 'app-home',
@@ -13,10 +15,25 @@ export class HomePage implements OnInit {
   //DuocPuente    -33.598208429252715, -70.57865623980268
   lat: number = -33.598208429252715;
   lng: number = -70.57865623980268;
+
+  //RUTA desde mi casa al duoc
+  origen={lat:-33.5751695 , lng:-70.60354650000001 }
+  destino={lat:-33.598208429252715 , lng:-70.57865623980268 }
+
   map=null;
+
+  //Crear un servicio de manejo de Ruta
+  direcionService = new google.maps.DirectionsService();
+  //crear un servicio de Render (dibujado de mapa)
+  direccionDibuja = new google.maps.DirectionsRenderer();
+  //crear variable donde recuperamos la direccion
+  dire:string;
+
   constructor(
     private geoLoca: Geolocation,
-    private loadingCtrl: LoadingController) { }
+    private loadingCtrl: LoadingController,
+    private api:ApiService
+    ) { }
   ngOnInit() {
     this.cargarMapa();
   }
@@ -34,10 +51,11 @@ export class HomePage implements OnInit {
       center: ubicacion,
       zoom: 20
     });
+    this.direccionDibuja.setMap(this.map);
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
       cargar.dismiss();
 
-      const marcador = new google.maps.Marker({
+      /*const marcador = new google.maps.Marker({
         position: {
           lat: ubicacion.lat,
           lng: ubicacion.lng
@@ -46,7 +64,24 @@ export class HomePage implements OnInit {
         map: this.map,
         title: 'Sede'
       });
-      this.cargarMarcardores();
+      this.cargarMarcardores();*/
+      this.calcularRuta();
+    });
+  }
+  //metodo que permite calcular una ruta
+  private calcularRuta(){
+    this.direcionService.route({
+      origin: this.origen,
+      destination:this.destino,
+      travelMode: google.maps.TravelMode.DRIVING,
+      waypoints: this.WayPoints,
+      optimizeWaypoints: true
+    },(response, status)=>{
+      if (status== google.maps.DirectionsStatus.OK) {
+        this.direccionDibuja.setDirections(response);
+      }else{
+        console.log("Error al cargar la ruta " + status );
+      }
     });
   }
   
@@ -67,13 +102,26 @@ export class HomePage implements OnInit {
       title:ubicacion.title
     });
   }
+  // Lista de puntos sobre paradas en mi ruta
+  WayPoints: WayPoint[]=[
+    {
+      location:{ 
+        lat:-33.58140964057867, lng:-70.59751299134992
+      },stopover:true
+    },
+    {
+      location:{ 
+        lat:-33.58006482405834, lng:-70.58731108736964
+      },stopover:true
+    }
+  ]
 
   listaMarcadores: MarcadorI[] = [{
     position: {
       lat: -33.59804332761758,
       lng: -70.5751251937849
     },
-    title: 'Colegio  San Carlos'
+    title: 'Colegio San Carlos'
   },
   {
     position: {
@@ -83,4 +131,16 @@ export class HomePage implements OnInit {
     title: 'Casita'
   }
   ];
+  //metodo que busca la direccion
+  private Direccion(){
+    console.log(this.dire);
+    this.api.getDireccion(this.dire).subscribe(
+      (data)=>{
+        console.log(data);
+      },
+      (e)=>{
+        console.log(e);
+      }
+    )
+  }
 }
